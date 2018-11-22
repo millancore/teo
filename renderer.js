43672f69c68ca71c560100648562662c15445c52
Juan Millan <juanes14569@hotmail.com>
2018-11-22 13:47:27 -0500

    add editor and syntax definition

--- renderer.js
+++ renderer.js
@@ -1,3 +1,51 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const path = require('path');
+const amdLoader = require('./node_modules/monaco-editor/min/vs/loader.js');
+const amdRequire = amdLoader.require;
+const Split = require('split.js');
+const syntaxOGL = require('./src/ogl/syntaxOGL');
+const PerfectScrollbar = require('perfect-scrollbar');
+
+const ps = new PerfectScrollbar('#editor');
+
+//const amdDefine = amdLoader.require.define;
+
+function uriFromPath(_path) {
+    var pathName = path.resolve(_path).replace(/\\/g, '/');
+    if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+        pathName = '/' + pathName;
+    }
+    return encodeURI('file://' + pathName);
+}
+
+amdRequire.config({
+    baseUrl: uriFromPath(path.join(__dirname, './node_modules/monaco-editor/min'))
+});
+
+// workaround monaco-css not understanding the environment
+self.module = undefined;
+
+
+amdRequire(['vs/editor/editor.main'], function () {
+
+    monaco.languages.register({
+        id: 'ogl'
+    });
+
+    monaco.languages.setMonarchTokensProvider('ogl', syntaxOGL);
+
+    var editor = monaco.editor.create(document.getElementById('editor'), {
+        value: [
+            'function x() {',
+            '\tconsole.log("Hello world!");',
+            '}'
+        ].join('\n'),
+        language: 'ogl',
+        theme: 'vs-dark'
+    });
+});
+
+
+Split(['#panel-editor', '#panel-view'], {
+    gutterSize: 8,
+    cursor: 'col-resize'
+});
